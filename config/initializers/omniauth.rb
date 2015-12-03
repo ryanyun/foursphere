7e1e645fbd876d0aecb7d2ef252f497e9972043c
ryannyunn <ryannyunn@gmail.com>
2015-12-03 11:24:24 -0500

    public keys now public for heroku

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :foursquare, ENV['fsq_client'], ENV['fsq_secret'], { 
+  provider :foursquare, 'Z0T5FAZ3AMXDGEI3KKVBAPL1OXJS5OTCOXYAX51XJ1XT51KZ', ENV['fsq_secret'], { 
     client_options: { ssl: { 
         ca_file: '/usr/lib/ssl/certs/ca-certificates.crt',
         ca_path: "/etc/ssl/certs"
