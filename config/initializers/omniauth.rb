9ce6af4794619234f7b8ac46c3f4ddac73aa38d8
ryannyunn <ryannyunn@gmail.com>
2015-12-03 11:17:55 -0500

    ca ctf issue

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :foursquare, ENV['fsq_client'], ENV['fsq_secret'],
-    {:client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
+  provider :foursquare, ENV['fsq_client'], ENV['fsq_secret'], { 
+    client_options: { ssl: { 
+        ca_file: '/usr/lib/ssl/certs/ca-certificates.crt',
+        ca_path: "/etc/ssl/certs"
+    }}
+  }
 end
